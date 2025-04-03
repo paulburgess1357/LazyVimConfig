cb77b1ef566ce67829022267ddbba0ccdfb22e7a
Paul B <paulburgess1357@gmail.com>
2025-04-03 08:39:46 -0600

    merge lsp stuff

--- lua/plugins/lsp_extend.lua
+++ lua/plugins/lsp_extend.lua
@@ -1,12 +1,22 @@
 return {
   "neovim/nvim-lspconfig",
   opts = {
+    -- Global LSP config
     inlay_hints = {
       enabled = false,
     },
     diagnostics = {
       virtual_text = false,
       signs = true,
+    },
+
+    setup = {
+      clangd = function(_, opts)
+        -- No need to check opts.cmd — just add the arg
+        table.insert(opts.cmd, "--limit-results=0")
+
+        return false -- continue with LazyVim's default LSP setup
+      end,
     },
   },
 }
