c18e6caf51964293d2ca8e3d04b5673e2dd9ba87
Paul B <paulburgess1357@gmail.com>
2025-04-03 08:18:40 -0600

    clean up lsp settings

--- lua/plugins/lsp_extend.lua
+++ lua/plugins/lsp_extend.lua
@@ -1,9 +1,12 @@
 return {
   "neovim/nvim-lspconfig",
   opts = {
+    inlay_hints = {
+      enabled = false,
+    },
     diagnostics = {
       virtual_text = false,
-      signs = true, -- Ensure signs are enabled
+      signs = true,
     },
   },
 }
