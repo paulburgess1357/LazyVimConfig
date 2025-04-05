6bbb584c0d00046c405dcf0da60761683232e2c5
Paul B <paulburgess1357@gmail.com>
2025-04-04 21:08:44 -0600

    add pyright

--- lua/plugins/lsp_extend.lua
+++ lua/plugins/lsp_extend.lua
@@ -9,12 +9,14 @@ return {
       virtual_text = false,
       signs = true,
     },
-
+    -- Add your LSP servers here:
+    servers = {
+      pyright = {}, -- pyright will be automatically installed with mason and loaded with lspconfig
+    },
     setup = {
       clangd = function(_, opts)
         -- No need to check opts.cmd — just add the arg
         table.insert(opts.cmd, "--limit-results=0")
-
         return false -- continue with LazyVim's default LSP setup
       end,
     },
