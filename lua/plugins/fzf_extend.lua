f0de157968664ac1376606538f08685d2aa4d326
Paul B <paulburgess1357@gmail.com>
2025-04-05 12:57:58 -0600

    Map open file

--- lua/plugins/fzf_extend.lua
+++ lua/plugins/fzf_extend.lua
@@ -4,12 +4,18 @@ return {
   opts = {
     files = {
       hidden = false,
+      actions = {
+        -- add a mapping so that Ctrl+l also opens the selected file
+        ["ctrl-l"] = require("fzf-lua").actions.file_edit_or_qf,
+      },
     },
     keymap = {
       builtin = {
-        true,
+        true, -- inherit default keymaps
         ["<C-d>"] = "preview-page-down",
         ["<C-u>"] = "preview-page-up",
+        ["C-k"] = "up",
+        ["C-j"] = "down",
       },
       fzf = { true },
     },
