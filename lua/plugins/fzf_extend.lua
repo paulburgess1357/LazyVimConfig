575c80775b4267379c7c25e8218ae5b31d75e4d2
Paul B <paulburgess1357@gmail.com>
2025-04-05 13:45:27 -0600

    commented out workign section

--- lua/plugins/fzf_extend.lua
+++ lua/plugins/fzf_extend.lua
@@ -1,3 +1,29 @@
+--[[
+return {
+  "ibhagwan/fzf-lua",
+  dependencies = { "nvim-tree/nvim-web-devicons" },
+  opts = {
+    files = {
+      hidden = false,
+      actions = {
+        -- add a mapping so that Ctrl+l also opens the selected file
+        ["ctrl-l"] = require("fzf-lua").actions.file_edit_or_qf,
+      },
+    },
+    keymap = {
+      builtin = {
+        true, -- inherit default keymaps
+        ["<C-d>"] = "preview-page-down",
+        ["<C-u>"] = "preview-page-up",
+        ["C-k"] = "up",
+        ["C-j"] = "down",
+      },
+      fzf = { true, ["ctrl-q"] = "abort" },
+    },
+  },
+}
+]]
+
 return {
   "ibhagwan/fzf-lua",
   dependencies = { "nvim-tree/nvim-web-devicons" },
