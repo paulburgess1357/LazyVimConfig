1d854223fc80c37fe2531fee0860a25ad60cbf9e
Paul B <paulburgess1357@gmail.com>
2025-04-05 22:20:51 -0600

    added todo

--- lua/plugins/fzf_extend.lua
+++ lua/plugins/fzf_extend.lua
@@ -1,28 +1,4 @@
---[[
-return {
-  "ibhagwan/fzf-lua",
-  dependencies = { "nvim-tree/nvim-web-devicons" },
-  opts = {
-    files = {
-      hidden = false,
-      actions = {
-        -- add a mapping so that Ctrl+l also opens the selected file
-        ["ctrl-l"] = require("fzf-lua").actions.file_edit_or_qf,
-      },
-    },
-    keymap = {
-      builtin = {
-        true, -- inherit default keymaps
-        ["<C-d>"] = "preview-page-down",
-        ["<C-u>"] = "preview-page-up",
-        ["C-k"] = "up",
-        ["C-j"] = "down",
-      },
-      fzf = { true, ["ctrl-q"] = "abort" },
-    },
-  },
-}
-]]
+-- TODO: Need to update fzf grep search to open files with my keymaps also
 
 return {
   "ibhagwan/fzf-lua",
