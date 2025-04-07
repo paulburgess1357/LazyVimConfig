426c350a99509343eeb75b0bf52ccd1ec8e5c578
Paul B <paulburgess1357@gmail.com>
2025-04-07 08:57:07 -0600

    testing defaults; updated todo

--- lua/plugins/fzf_extend.lua
+++ lua/plugins/fzf_extend.lua
@@ -1,20 +1,29 @@
 -- TODO: Need to update fzf grep search to open files with my keymaps also
-
+--
 return {
   "ibhagwan/fzf-lua",
   dependencies = { "nvim-tree/nvim-web-devicons" },
+  defaults = {
+    -- Override the default actions for all pickers so that both single- and multi-select
+    -- always call file_edit rather than falling back to the quickselect list.
+    actions = {
+      ["default"] = require("fzf-lua.actions").file_edit,
+      ["enter"] = require("fzf-lua.actions").file_edit,
+      ["ctrl-l"] = require("fzf-lua.actions").file_edit,
+    },
+  },
   opts = {
     files = {
       hidden = false,
       actions = {
-        -- add a mapping so that Ctrl+l also opens the selected file
+        -- For the files picker we can also explicitly set these if desired.
         ["ctrl-l"] = require("fzf-lua.actions").file_edit,
         ["default"] = require("fzf-lua.actions").file_edit,
       },
     },
     keymap = {
       builtin = {
-        true, -- inherit default keymaps
+        false, -- do not inherit default keymaps
         ["<C-d>"] = "preview-page-down",
         ["<C-u>"] = "preview-page-up",
         ["C-k"] = "up",
