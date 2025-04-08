65dfdead9d0935a63df7472c2f2e2217de346d1c
Paul B <paulburgess1357@gmail.com>
2025-04-07 21:09:31 -0600

    add note

--- lua/plugins/snacks_dashboard_extend.lua
+++ lua/plugins/snacks_dashboard_extend.lua
@@ -1,3 +1,7 @@
+-- Note: If future bug appears where fzf-lua does not work with ctrl-j and ctrl-k for directions, try removing this file.
+-- I have not confirmed, but I briefly had a bug where right after I added this file, the directions did not work.  I'm unsure
+-- if its related to this or not.
+
 return {
   "snacks.nvim",
   opts = {
