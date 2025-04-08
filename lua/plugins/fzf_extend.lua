0ba0514ab1289a926c70e09a622271b7b6712f80
Paul B <paulburgess1357@gmail.com>
2025-04-07 19:32:05 -0600

    lsp working

--- lua/plugins/fzf_extend.lua
+++ lua/plugins/fzf_extend.lua
@@ -1,11 +1,11 @@
 -- TODO: Need to update fzf grep search to open files with my keymaps also
---
+
 return {
   "ibhagwan/fzf-lua",
   dependencies = { "nvim-tree/nvim-web-devicons" },
   defaults = {
     -- Override the default actions for all pickers so that both single- and multi-select
-    -- always call file_edit rather than falling back to the quickselect list.
+    -- always open the file rather than falling back to the quickfix list.
     actions = {
       ["default"] = require("fzf-lua.actions").file_edit,
       ["enter"] = require("fzf-lua.actions").file_edit,
@@ -16,9 +16,22 @@ return {
     files = {
       hidden = false,
       actions = {
-        -- For the files picker we can also explicitly set these if desired.
-        ["ctrl-l"] = require("fzf-lua.actions").file_edit,
+        -- For the files picker the actions are explicitly set
         ["default"] = require("fzf-lua.actions").file_edit,
+        ["ctrl-l"] = require("fzf-lua.actions").file_edit,
+      },
+    },
+    -- Override for grep mode to prevent the quickfix list when multi-selecting
+    grep = {
+      actions = {
+        ["default"] = require("fzf-lua.actions").file_edit,
+        ["ctrl-l"] = require("fzf-lua.actions").file_edit,
+      },
+    },
+    lsp = {
+      actions = {
+        ["default"] = require("fzf-lua.actions").file_edit,
+        ["ctrl-l"] = require("fzf-lua.actions").file_edit,
       },
     },
     keymap = {
