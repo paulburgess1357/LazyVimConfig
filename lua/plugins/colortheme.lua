9474c78c16b82b873b9bdb09369f696d7d413891
Paul B <paulburgess1357@gmail.com>
2025-04-08 21:42:24 -0600

    fixed separater

--- lua/plugins/colortheme.lua
+++ lua/plugins/colortheme.lua
@@ -25,15 +25,4 @@ return {
     "LazyVim/LazyVim",
     opts = { colorscheme = "kanagawa-wave" },
   },
-  {
-    "nvim-lualine/lualine.nvim", -- If you have lualine
-    optional = true,
-    opts = function(_, opts)
-      -- Make status line separators more visible
-      opts.options = opts.options or {}
-      opts.options.section_separators = { left = "", right = "" }
-      opts.options.component_separators = { left = "│", right = "│" }
-      return opts
-    end,
-  },
 }
