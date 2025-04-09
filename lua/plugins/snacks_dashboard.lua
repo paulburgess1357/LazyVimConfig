160ff2039a294dd10d86a9b8915257b908ce01f0
Paul B <paulburgess1357@gmail.com>
2025-04-08 20:37:03 -0600

    colors

--- lua/plugins/snacks_dashboard.lua
+++ lua/plugins/snacks_dashboard.lua
@@ -1,7 +1,6 @@
 -- Note: If future bug appears where fzf-lua does not work with ctrl-j and ctrl-k for directions, try removing this file.
 -- I have not confirmed, but I briefly had a bug where right after I added this file, the directions did not work.  I'm unsure
 -- if its related to this or not.
-
 return {
   "snacks.nvim",
   opts = {
@@ -23,4 +22,23 @@ return {
       },
     },
   },
+  config = function(_, opts)
+    -- First setup the dashboard
+    require("snacks").setup(opts)
+
+    -- Define custom darker background and new logo color
+    -- Using a darker background and a different logo color
+    local bg_color = "#16161D" -- Darker background (sumiInk0)
+    local logo_color = "#E6C384" -- Logo color (carpYellow)
+
+    -- Then define the highlight groups for the dashboard with new colors
+    vim.api.nvim_set_hl(0, "SnacksDashboardNormal", { fg = "#DCD7BA", bg = bg_color })
+    vim.api.nvim_set_hl(0, "SnacksDashboardHeader", { fg = logo_color, bg = bg_color }) -- New logo color
+    vim.api.nvim_set_hl(0, "SnacksDashboardFooter", { fg = "#7E9CD8", bg = bg_color }) -- Wave Blue
+    vim.api.nvim_set_hl(0, "SnacksDashboardIcon", { fg = "#DCA561", bg = bg_color }) -- Roninc Yellow
+    vim.api.nvim_set_hl(0, "SnacksDashboardKey", { fg = "#FFA066", bg = bg_color }) -- Autumn Yellow
+    vim.api.nvim_set_hl(0, "SnacksDashboardDesc", { fg = "#957FB8", bg = bg_color }) -- Light Purple
+    vim.api.nvim_set_hl(0, "SnacksDashboardDir", { fg = "#76946A", bg = bg_color }) -- Directory paths
+    vim.api.nvim_set_hl(0, "SnacksDashboardFile", { fg = "#C8C093", bg = bg_color }) -- File names
+  end,
 }
