740389c4e66a7cb36bc217e00a60e2274e0c53be
Paul B <paulburgess1357@gmail.com>
2025-04-08 21:40:08 -0600

    early working need tod elete some

--- lua/plugins/colortheme.lua
+++ lua/plugins/colortheme.lua
@@ -6,16 +6,53 @@ return {
         theme = {
           wave = {
             ui = {
-              bg = "#16161D", -- Set the darker background color
+              bg = "#16161D", -- Your darker background color
               bg_gutter = "#16161D",
             },
           },
         },
       },
+      overrides = function(colors)
+        local theme = colors.theme
+        return {
+          -- Make window borders much more visible
+          WinSeparator = { fg = colors.palette.autumnYellow, bold = true },
+        }
+      end,
     },
   },
   {
     "LazyVim/LazyVim",
     opts = { colorscheme = "kanagawa-wave" },
+  },
+  {
+    "nvim-lualine/lualine.nvim", -- If you have lualine
+    optional = true,
+    opts = function(_, opts)
+      -- Make status line separators more visible
+      opts.options = opts.options or {}
+      opts.options.section_separators = { left = "", right = "" }
+      opts.options.component_separators = { left = "│", right = "│" }
+      return opts
+    end,
+  },
+  {
+    "folke/which-key.nvim",
+    optional = true,
+    config = function(_, opts)
+      -- Add this chunk to set more visible window separators
+      local wk = require("which-key")
+      wk.setup(opts)
+      -- Set up thicker window separators
+      vim.opt.fillchars = {
+        horiz = "━",
+        horizup = "┻",
+        horizdown = "┳",
+        vert = "┃",
+        vertleft = "┫",
+        vertright = "┣",
+        verthoriz = "╋",
+      }
+    end,
   },
 }
