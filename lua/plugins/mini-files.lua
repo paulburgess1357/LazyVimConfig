0761357f06b8e76ae2e86360172257071b5f6d81
Paul B <paulburgess1357@gmail.com>
2025-04-09 21:07:07 -0600

    mini-files color

--- lua/plugins/mini-files.lua
+++ lua/plugins/mini-files.lua
@@ -36,12 +36,22 @@ return {
   config = function(_, opts)
     require("mini.files").setup(opts)
 
-    -- Add Enter key mapping to do the same as 'l'
+    -- Remap Enter to behave like 'l'
     vim.api.nvim_create_autocmd("User", {
       pattern = "MiniFilesBufferCreate",
       callback = function(args)
         vim.keymap.set("n", "<CR>", "l", { buffer = args.data.buf_id, remap = true })
       end,
     })
+
+    -- 🟡 Use warm yellow colors from Kanagawa
+    local kanagawa = require("kanagawa.colors").setup()
+    local p = kanagawa.palette
+
+    -- Apply highlights
+    vim.api.nvim_set_hl(0, "MiniFilesNormal", { fg = p.oldWhite, bg = "#16161D" }) -- File text
+    vim.api.nvim_set_hl(0, "MiniFilesDirectory", { fg = p.carpYellow, bold = true }) -- Directories
+    vim.api.nvim_set_hl(0, "MiniFilesCursorLine", { bg = p.sumiInk3 }) -- Highlight current line
+    vim.api.nvim_set_hl(0, "MiniFilesBorder", { fg = p.autumnYellow, bg = "#16161D" }) -- Border with yellow
   end,
 }
